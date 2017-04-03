0fed5ddbe5ac3c0bad246f4cb44cd229f5d6488d
uniibu <unibtc@gmail.com>
2017-04-03 22:36:40 +0800

    Update index.js
    update for use with koa@2

--- index.js
+++ index.js
@@ -1,42 +1,41 @@
-var nunjucks = require('nunjucks');
+"use strict";
+const nunjucks = require('nunjucks');
 
 // opts are passed direct to nunjucks
 // with an additional 'ext' for extention
 //  (a common suffix for templates)
 module.exports = function(path, opts) {
-  var env = nunjucks.configure(path, opts);
+  let env = nunjucks.configure(path, opts);
 
-  var ext = opts.ext || '';
+  const ext = opts.ext || '';
 
-  var filters = opts.filters || {};
-  for (var f in filters) {
-    env.addFilter(f, filters[f]);
-  }
+  const filters = opts.filters || {};
+  Object.keys(filters).forEach(f => {
+    env.addFilter(f, globals[f]);
+  });
 
-  var globals = opts.globals || {};
-  for (var g in globals) {
+  const globals = opts.globals || {};
+  Object.keys(globals).forEach(g => {
     env.addGlobal(g, globals[g]);
-  }
+  });
 
-  var extensions = opts.extensions || {};
-  for (var e in extensions) {
-    env.addExtension(e, extensions[e]);
-  }
+  const extensions = opts.extensions || {};
+  Object.keys(extensions).forEach(e => {
+    env.addExtension(e, globals[e]);
+  });
 
-  return function*(next) {
-    var self = this;
-
-    this.render = function(view, context) {
-      return new Promise(function(resolve, reject) {
-        nunjucks.render(view+ext, context, function(err, body) {
+  return (ctx, next) => {
+    ctx.render = (view, context) => {
+      return new Promise((resolve, reject) => {
+        nunjucks.render(view+ext, context, (err, body) => {
           if (err)
             return reject(err);
-          self.body = body;
+          ctx.body = body;
           resolve();
         });
       });
     };
 
-    yield* next;
-  }
-}
+    return next();
+  };
+};
